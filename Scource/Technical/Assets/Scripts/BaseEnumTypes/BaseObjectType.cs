fab5a391c8c5b2291fd432402013388fae17fc63
toneylee <ducsangpro.93@gmail.com>
2015-10-25 11:52:43 +0700

    Base Item, them thu muc, doi tuong

--- Scource/Technical/Assets/Scripts/BaseEnumTypes/BaseObjectType.cs
+++ Scource/Technical/Assets/Scripts/BaseEnumTypes/BaseObjectType.cs
@@ -7,14 +7,18 @@ public enum BaseObjectType
     OB_ENEMY = 1,
     OB_WALL = 2,
     OB_BULLET = 3,
-    OB_OTHER = 4,
+    OB_ITEM = 4,
+    OB_OTHER = 5,
     //
-    OBE_ENEMY_TANKER = 5,
-    OBE_ENEMY_OTHER = 6,
+    OBE_ENEMY_TANKER = 6,
+    OBE_ENEMY_OTHER = 7,
     // particaL
     OBP_EXPLOSION_C_E_B = 10,
     OBP_EXPLOSION_GUN_SUPPORT = 11,
     OBP_ENEMY_DIE = 12,
-    OBP_WALL_FALL = 13
+    OBP_WALL_FALL = 13,
+    //UI
+    OU_COIN = 20
+    
 
 }
