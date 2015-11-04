8cf20bbbf847edd3d1b4f40308dc202ddd7348c2
tobi994 <viettt.fuky@gmail.com>
2015-11-04 11:10:20 +0700

    contain: + Flight Release Item + add foder Prefabs/Object/Enemy/Flight.prefab

---
+++ Scource/Technical/Assets/Scripts/ObjectClass/Item/Item1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item1 : BaseItemObject {
+    public override void InitObject()
+    {
+        base.InitObject();
+    }
+    public override void Move()
+    {
+        positionBegin = transform.position;
+        switch(direction)
+        {
+            case BaseDirectionType.DOWN:
+                positionBegin.y += vy * Time.deltaTime;
+                break;
+        }
+        transform.position = positionBegin;
+    }
+    public override void UpdateObject()
+    {
+        Move();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+    }
+}
